efc83165713ef1f70f4c4c8610aaf8764a1fc935
svetli9805 <ssvetoslavvv@gmail.com>
2024-09-21 21:58:25 +0300

    setup UI

---
+++ opteamer-server/src/main/java/io/medsys/opteamer/OpteamerApplication.java
@@ -0,0 +1,13 @@
+package io.medsys.opteamer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpteamerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpteamerApplication.class, args);
+	}
+
+}
---
+++ opteamer-server/src/test/java/io/medsys/opteamer/OpteamerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.medsys.opteamer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpteamerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
